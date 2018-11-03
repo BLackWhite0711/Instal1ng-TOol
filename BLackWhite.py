10484c0a0b61808d9bc68f2930b86fbcc4441be9
BLackWhite0711 <43264850+BLackWhite0711@users.noreply.github.com>
2018-11-03 09:39:24 +0700

    BLackWhite.py
    Instaling

---
+++ BLackWhite.py
@@ -0,0 +1,126 @@
+biru = ' \ 033 [34; 1m '
+
+hijau = ' \ 033 [32; 1m '                                        
+
+ungu = ' \ 033 [35; 1m '
+
+cyan = ' \ 033 [36; 1m '
+
+merah = ' \ 033 [31; 1m '
+
+putih = ' \ 033 [37; 1m '                                           
+
+kuning = ' \ 033 [33; 1m '
+
+tidur 1
+
+toilet -f big -F gay " T00Ls "  | lolcat
+
+toilet -f big -F gay " BLackWhite " 
+
+echo  " "      
+
+echo  " \ 033 [32; 1mAuthor: Mr.BLackWhite " 
+
+echo  " \ 033 [35; 1mTeam: BLackWhite TEAM "
+
+echo  " \ 033 [33; 1mKontak Me: 089629191142 "
+
+echo  " \ 033 [31; 1mSubcribe Youtube: "
+
+echo  " \ 033 [33; 1mBLackWhite Team"  " \ 033 [32; 1m & "  " \ 033 [36; ANONYMOUS "
+
+echo  " \ 033 [34; 1m [============================ [> "
+
+tidur 1
+
+echo  " \ 033 [32; 1mSilahkan Pilih Tools Yang mau Kamu pake :) ?: "
+
+echo  " \ 033 [36; 1m "
+
+echo  " [=============================================== [> "
+
+echo  $ green  " 1.> Nuyul KubikNews "
+
+echo  " [=============================================== [> "
+
+echo  $ red  " 2.> Nuyul Veeu "
+
+echo  " [=============================================== [> "
+
+echo  $ blue  "0.> Exit
+
+echo  " [=============================================== [> "
+
+echo  $ yellow  "00.> Back
+
+jika [ $ bro  = 1] || [ $ bro  = 1]
+
+kemudian
+
+bersih
+
+figlet " BLackWhite "  | lolcat
+
+git clone https://github.com/adidoank/kubik
+
+cd kubik
+
+nano cfg.php
+
+fi
+
+jika [ $ bro  = 2] || [ $ bro  = 2]
+
+kemudian
+
+bersih
+
+figlet " BLackWhite "  | lolcat
+
+git clone https://github.com/zimbabwe2001/veeu
+
+cd veeu
+
+ls
+
+php veeu.php
+
+fi
+
+jika [ $ bro  = 0] || [ $ bro  = 00]
+
+kemudian
+
+echo  " \ 033 [32; BLackWhite "
+
+tidur 1
+
+echo  " \ 033 [33; Tidak Ada system yg aman "
+
+tidur 1
+
+echo  " We Not Friends "
+
+tidur 1
+
+echo  " ANONYMOUS "
+
+tidur 1
+
+echo  " BERSENANG-SENANGLAH DIDUNIA NYATA "
+
+tidur 1
+
+echo  " Incar lah yang Mustahil "
+
+tidur 1
+
+echo  " Jangan Ragu Untuk Memulai:) "
+
+tidur 1
+
+echo  " \ 033 [32; 1m Kepercayaan sayang di Butuhkan :) "
+
+tidur 1
+
