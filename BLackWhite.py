c89739da044ffab742ac847ca9b7c28a83507d5e
BLackWhite <43264850+BLackWhite0711@users.noreply.github.com>
2018-11-03 18:39:14 +0700

    Create BLackWhite.py

---
+++ BLackWhite.py
@@ -0,0 +1,47 @@
+## Hayo Mau Ngapain Lu ##
+
+### Copy Paste ? Mikir!! ###
+
+#### kembangkan Otak mu ####
+
+clear
+
+blue='\033[34;1m'
+
+green='\033[32;1m'                                        
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m'                                           
+
+yellow='\033[33;1m'
+
+sleep 1
+
+toilet -f big -F gay "TEAM" | lolcat
+
+toilet -f big -F gay "BLACKWHITE" 
+
+echo ""      
+
+echo "\033[32;1m Author : Noprian&Dilla" 
+
+echo "\033[35;1m Team : BLack White"
+
+echo "\033[33;1m Whatshapp :  089629191142 & 0895422976089"
+
+echo "\033[31;1m Subcribe Youtube: BLackWhite Team"
+
+echo "\033[33;1m BLackWhiteTeam" "\033[32;1m&" "\033[36;1mTidak Ada system yang aman"
+
+echo "\033[34;1m [============================[>"
+
+sleep 1
+
+echo ""
+
+echo "\033[32;1m JANGAN BINGUNG Di COBA AJA :):"
